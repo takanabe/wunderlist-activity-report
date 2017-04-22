9de627080c37470cafed11538b4efc499158a6f0
Takayuki WATANABE <takayuki-watanabe@cookpad.com>
2017-04-22 18:06:59 +0900

    Enable spec

--- lib/wunderlist/activity/report/version.rb
+++ lib/wunderlist/activity/report/version.rb
@@ -1,7 +1,7 @@
 module Wunderlist
   module Activity
     module Report
-      VERSION = "0.1.0"
+      VERSION = "0.0.1"
     end
   end
 end
--- spec/wunderlist/activity/report_spec.rb
+++ spec/wunderlist/activity/report_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Wunderlist::Activity::Report do
   it "has a version number" do
     expect(Wunderlist::Activity::Report::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
